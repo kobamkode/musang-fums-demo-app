35cf1b6f463321c5ceef46695ddc5dea3acf1202
Mario Wibisono <kobamkode@gmail.com>
2025-09-13 19:32:39 +0700

    add fixed station info

--- src/routes/(app)/fixed/details/+page.svelte
+++ src/routes/(app)/fixed/details/+page.svelte
@@ -13,9 +13,29 @@
 			flowmeters: Flowmeter[];
 		};
 	} = $props();
+	const latest = data.flowmeters[0];
 </script>
 
 <div class="flex flex-col gap-4">
+	<Card>
+		<CardHeader class="text-xl">Fixed Station Info</CardHeader>
+		<CardContent>
+			<div class="border-t pt-4 lg:border-t-0 lg:border-l lg:pt-0 lg:pl-4">
+				<table>
+					<tbody>
+						<tr>
+							<td class="py-1 pr-4 text-muted-foreground">Panel Id</td>
+							<td>{latest.panel_id}</td>
+						</tr>
+						<tr>
+							<td class="py-1 pr-4 text-muted-foreground">Location</td>
+							<td>{latest.location}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</CardContent>
+	</Card>
 	<Card>
 		<CardHeader class="text-xl">Transactions</CardHeader>
 		<CardContent>
