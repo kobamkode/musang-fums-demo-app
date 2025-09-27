7dbc6214a967a885eb051aa623aae132c1963ada
Mario Wibisono <kobamkode@gmail.com>
2025-09-27 16:39:20 +0700

    add ifuel fixed calculation

--- src/routes/(app)/section-fixed.svelte
+++ src/routes/(app)/section-fixed.svelte
@@ -4,14 +4,12 @@
 	const { fixedStat } = $props();
 </script>
 
-<div
-	class="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card"
->
+<div class="w-72">
 	<Card.Root class="@container/card">
 		<Card.Header>
 			<Card.Description>{fixedStat.location}</Card.Description>
 			<Card.Title class="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-				{fixedStat.calculation}
+				{fixedStat.calculation} ℓ
 			</Card.Title>
 		</Card.Header>
 	</Card.Root>
